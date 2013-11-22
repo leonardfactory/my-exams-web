93d2bdf5eb973f99085a152adcc38363d0e02b32
leonardfactory <me@leonardfactory.com>
2013-11-22 20:18:31 +0100

    Grunt, Responsive grid + basic css, some HTML

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+		'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+		'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+		'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+		' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+		// Task configuration.
+		concat: {
+			options: {
+				banner: '<%= banner %>',
+				stripBanners: true
+			},
+			dist: {
+				src: ['src/<%= pkg.name %>.js'],
+				dest: 'js/<%= pkg.name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>'
+			},
+			dist: {
+				src: '<%= concat.dist.dest %>',
+				dest: 'js/<%= pkg.name %>.min.js'
+			}
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				unused: true,
+				boss: true,
+				eqnull: true,
+				browser: true,
+				globals: {}
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			}
+		},
+		compass: {                  // Task
+			dist: {                   // Target
+				options: {              // Target options
+					sassDir: 'scss',
+					cssDir: 'css',
+					environment: 'development'
+				}
+			},
+			dev: {                    // Another target
+				options: {
+					sassDir: 'sass',
+					cssDir: 'css'
+				}
+			}
+		},
+		watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: ['jshint:gruntfile']
+			},
+			sass_styles: {
+				files: 'scss/*.scss',
+				tasks: ['compass']
+			},
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'compass', 'concat', 'uglify']);
+
+};
